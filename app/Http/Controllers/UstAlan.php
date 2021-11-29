1aee4f8038e49dd95ecd919c966b3beb6b1e7af8
Mehtap <75047473+MehtapAyall@users.noreply.github.com>
2021-11-29 12:48:15 +0300

    kaydetme

---
+++ app/Http/Controllers/UstAlan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UstAlan extends Controller
+{
+    public function goster(){
+      return view("ustalan");
+    }
+}
